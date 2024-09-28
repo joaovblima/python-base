a485410aec5f1746a51ed4534822e6be2164b108
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-09-28 13:57:28 -0300

    adiciona doc e implementação do hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 
-print("hello world")
+"""
+Ola mundo de acordo com a variavel de ambiente
+
+Modo de uso:
+Tenha uma variável LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py 
+    ou 
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "João Lima"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+
+print(msg)
